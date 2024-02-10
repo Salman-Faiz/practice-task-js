4b2be2a62047d52487194e3af77a1d8bbdcf3fdc
Salman-Faiz <salmanfaiz.cse@gmail.com>
2024-02-10 11:23:01 +0600

    using an array in a function to find out average value

---
+++ task-3.js
@@ -0,0 +1,8 @@
+make_array=[15,15,60];
+function make_avg(make_array){
+var sum = make_array[0]+make_array[1]+make_array[2];
+var average = sum/3;
+console.log(average);
+return average;    
+}
+make_avg(make_array);
