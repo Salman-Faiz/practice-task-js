f80a712f5a28715133e685ca14007053d66ba70c
Salman-Faiz <salmanfaiz.cse@gmail.com>
2024-02-13 23:55:38 +0600

    check dataType using type of method

--- task-18.js
+++ task-18.js
@@ -4,3 +4,13 @@ let a=[1,2];
 let b=[3,4];
 let result =[a,b]=[b,a]
 console.log( a,b);
+
+// to search the type ,include should be used
+let c =2;
+let d=true;
+let e={ id: 9, named : 'rahim' };
+console.log(Array.isArray(a));
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+
